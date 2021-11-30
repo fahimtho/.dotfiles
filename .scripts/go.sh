05be3147787d460b311a224fa1cbb3815cff35c2
Fahim Faisal <fahimfaisal.fad@gmail.com>
2021-12-01 00:28:23 +0600

    fix

--- .scripts/go.sh
+++
@@ -1,3 +0,0 @@
-go=$(bspc query -D -d .local --names | rofi -dmenu -theme ~/.scripts/workspace.rasi -no-fixed-num-lines -yoffset -0  -i -kb-accept-entry "!Super-Escape,!Alt+Alt_L" -kb-row-down "Super+Escape" -kb-row-up "Super+F1")
-bspc desktop --focus $go
-
--- .scripts/workspace.sh
+++
@@ -1,7 +0,0 @@
-active=$(bspc query -D -d .occupied --names | rofi -dmenu -theme ~/.scripts/active.rasi -no-fixed-num-lines -yoffset "-0" -kb-accept-entry "!Alt-Escape,!Alt+Alt_L" -kb-row-down "Alt+Escape" -kb-row-up "Alt+F1" )
-
-bspc desktop --focus $active
-
-
-
-
